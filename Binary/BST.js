adccb21521395d904b5dcb2afa53d2b010eac3cd
xuyu <xypisces@163.com>
2018-11-03 14:49:23 +0800

    bst

--- Binary/BST.js
+++ Binary/BST.js
@@ -1,32 +1,32 @@
 class BTS {
-  constructor(key,value){
-    this.key = key
-    this.value = value
-    this.left = null
-    this.right = null
-    this.count = 0
-    this.node = null  //二叉树
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.count = 0;
+    this.node = null; //二叉树
   }
 
   insert(key, value) {
-    node = __insert(node, key, value)
+    node = __insert(node, key, value);
   }
   //向以node为根的二叉搜索树中，插入节点（key,value）返回插入新节点后的二叉搜索树的根
-  __insert(node=null, key, value) {
-    if( node == null) {
-      count++
+  __insert(node = null, key, value) {
+    if (node == null) {
+      count++;
       return {
         key,
-        value,
-      }
+        value
+      };
     }
-    if( key == node.key) {
-      node.value = value
-    }else if( key < node.key){
-      node.left = this.__insert(node.left, key, value)
-    }else{
-      node.right = this.__insert(node.right, key, value)
+    if (key == node.key) {
+      node.value = value;
+    } else if (key < node.key) {
+      node.left = this.__insert(node.left, key, value);
+    } else {
+      node.right = this.__insert(node.right, key, value);
     }
-    return node
+    return node;
   }
 }
--- Binary/BinarySearch.js
+++ Binary/BinarySearch.js
@@ -1,17 +1,18 @@
 // 二分查找,若存在则返回值的索引，若不存在则返回-1
-function binarySearch (arr, target) {
+function binarySearch(arr, target) {
   const n = arr.length;
-  let l = 0, r = n - 1;
-  while( l<= r) {
+  let l = 0,
+    r = n - 1;
+  while (l <= r) {
     // let mid = (l+r) / 2
-    let mid = l + (r-l) /2
-    if(arr[mid] === target) {
-      return mid
+    let mid = l + (r - l) / 2;
+    if (arr[mid] === target) {
+      return mid;
     }
-    if(target < arr[mid]) {
-      r = mid - 1
-    }else {
-      l = mid + 1
+    if (target < arr[mid]) {
+      r = mid - 1;
+    } else {
+      l = mid + 1;
     }
   }
   return -1;
