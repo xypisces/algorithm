b53b46dd84b97f1e83fb0d5feb7e151cf2831748
xuyu <xypisces@163.com>
2021-06-28 19:59:33 +0800

    feat: tree

--- Leetcode/Tree/450.删除二叉搜索树中的节点.js
+++ Leetcode/Tree/450.删除二叉搜索树中的节点.js
@@ -73,7 +73,25 @@
  * @return {TreeNode}
  */
 var deleteNode = function(root, key) {
-    
+    function callDFS(node) {
+      if(!node) return null;
+      if(node.val === key) {
+        if(!node.left && !node.right) return null;
+        if(!node.left) return node.right;
+        if(!node.right) return node.left;
+        let curr = node.right;
+        while(curr.left)  curr = curr.left;
+        curr.left = node.left;
+        return node.right;
+      }
+      if(key > node.val) {
+        node.right = callDFS(node.right)
+      } else {
+        node.left = callDFS(node.left)
+      }
+      return node;
+    }
+    return callDFS(root)
 };
 // @lc code=end
 
