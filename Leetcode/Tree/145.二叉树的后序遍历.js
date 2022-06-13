f833df285dd20d7ca674bebca6d0b015f4087ca9
xuyu <xuyusz@codemao.cn>
2022-06-13 11:21:42 +0800

    feat: postordertraversal

---
+++ Leetcode/Tree/145.二叉树的后序遍历.js
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=javascript
+ *
+ * [145] 二叉树的后序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.88%)
+ * Likes:    865
+ * Dislikes: 0
+ * Total Accepted:    454.3K
+ * Total Submissions: 598.6K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给你一棵二叉树的根节点 root ，返回其节点值的 后序遍历 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[3,2,1]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点的数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * 
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+  // let res = [];
+  // if(!root) return res;
+  // res = res.concat(postorderTraversal(root.left));
+  // res = res.concat(postorderTraversal(root.right));
+  // res.push(root.val);
+  // return res;
+  let res = [];
+  if(root === null) return res;
+  let stack = [];
+  let cur = root;
+  while(cur || stack.length) { 
+    while(cur) {
+      res.push(cur.val);
+      stack.push(cur);
+      cur = cur.right;
+    }
+    cur = stack.pop();
+    cur = cur.left;
+  }
+  return res.reverse()
+};
+// @lc code=end
+
