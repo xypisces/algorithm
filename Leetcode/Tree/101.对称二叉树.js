4fd0df0ff50ef5fe7413606108da5373c9cd50b4
xypisces <xypisces@163.com>
2022-06-22 15:03:12 +0800

    feat: complete fn

--- Leetcode/Tree/101.对称二叉树.js
+++ Leetcode/Tree/101.对称二叉树.js
@@ -60,7 +60,17 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-
+  if(root == null) return true;
+  return symmetryChecker(root.left, root.right);
 };
+
+function symmetryChecker(left, right) {
+  if(left == null && right == null) return true;
+  if(left == null || right == null) return false;
+  if(left.val !== right.val) return false;
+
+  return symmetryChecker(left.left, right.right)
+  && symmetryChecker(left.right, right.left)
+}
 // @lc code=end
 
